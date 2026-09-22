03fa5898b2616b2b65855d554f873eb9e6546b00
Blazej <exeenkontakt@gmail.com>
2026-09-22 14:46:48 +0200

    Hello Michal

--- hello/src/main/java/org/example/Main.java
+++ hello/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello, Karol");
+            System.out.println("Hello, Michal");
 
         }
     }

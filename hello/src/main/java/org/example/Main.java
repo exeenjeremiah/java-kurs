25b57d44d328d985435d58c8ea44cf18c986839c
Blazej <exeenkontakt@gmail.com>
2026-09-23 14:15:25 +0200

    feat: change name to Marlena

--- hello/src/main/java/org/example/Main.java
+++ hello/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello, Kacper and Anna");
+            System.out.println("Hello Marlena");
 
         }
     }

36f4b3732ffcd1b6d66fb86d9c6f772c355c2058
Blazej <exeenkontakt@gmail.com>
2026-09-24 15:14:58 +0200

    feat: change name to Marzena

--- hello/src/main/java/org/example/Main.java
+++ hello/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello Monika");
-            System.out.println("Hello Marzena");
+        System.out.println("Hello Marzena");
 
-        }
     }
+}
